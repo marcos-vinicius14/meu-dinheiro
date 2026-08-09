e12f62a77db42b0680cb48a4a862763ff0b42c7c
marcos <marcos.vinicius.dev3@gmail.com>
2026-08-08 22:04:38 -0300

    initial commit

---
+++ src/test/java/com/marcos/meudinheiro/MeuDinheiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.meudinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDinheiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
